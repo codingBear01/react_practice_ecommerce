a5aecec343e21d832ac29645c1b37e5e709b1d44
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-20 19:13:14 +0900

    working on Announce component

---
+++ src/components/Announce.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Close } from '@material-ui/icons';
+
+function Announce() {
+  return (
+    <div>
+      <h2>Hurry up! it's 40% off now</h2>
+      <Close />
+    </div>
+  );
+}
+
+export default Announce;
