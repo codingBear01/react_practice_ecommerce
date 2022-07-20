6a66dfd1aaa6c061f1da8d475d595006a3cbfbe0
codingBear <95178119+codingBear01@users.noreply.github.com>
2022-07-21 08:46:30 +0900

    feature navbar left

---
+++ src/components/Navabar.jsx
@@ -0,0 +1,26 @@
+import { React, useState } from 'react';
+import { Search } from '@material-ui/icons';
+
+function Navbar() {
+  return (
+    <div
+      className="navbar h-[60px] shadow-md
+  relative z-10"
+    >
+      <div className="wrapper pl-[20px] pr-[20px] pt-[10px] pb-[10px] flex justify-between items-center">
+        <div className="left flex flex-1 items-center">
+          <div className="language cursor-pointer text-[16px]">En</div>
+          <div class="searchInput">
+            <input className="input" type="text" />
+            <Search />
+          </div>
+        </div>
+
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Announce from './../components/Announce';
+import Navbar from './../components/Navabar';
 
 function Home() {
   return (
     <div>
       <Announce />
+      <Navbar />
     </div>
   );
 }
